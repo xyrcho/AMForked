655574320731e7bf484bdeccf76f9b4c145aa840
xʏʀᴄʜᴏ <98100800+xyrcho@users.noreply.github.com>
2022-07-08 16:41:04 +0700

    Update videocalls.py

--- Ayiin/Inline/videocalls.py
+++ Ayiin/Inline/videocalls.py
@@ -6,17 +6,17 @@ def choose_markup(videoid, duration, user_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text="⍟ 𝙼𝚄𝚂𝙸𝙲 ⍟",
+                text="ᴍᴜꜱɪᴄ",
                 callback_data=f"MusicStream {videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="⍟ 𝚅𝙸𝙳𝙴𝙾 ⍟",
+                text="ᴠɪᴅᴇᴏ",
                 callback_data=f"Choose {videoid}|{duration}|{user_id}",
             ),
         ],
         [
             InlineKeyboardButton(
-                text="⍟ 𝙲𝙻𝙾𝚂𝙴 ⍟",
+                text="ᴄʟᴏꜱᴇ",
                 callback_data=f"forceclose {videoid}|{user_id}",
             )
         ],
@@ -28,11 +28,11 @@ def livestream_markup(quality, videoid, duration, user_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text="⍟ 𝙻𝙸𝚅𝙴 ⍟",
+                text="ʟɪᴠᴇ",
                 callback_data=f"LiveStream {quality}|{videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="⍟ 𝙲𝙻𝙾𝚂𝙴 ⍟",
+                text="ᴄʟᴏꜱᴇ",
                 callback_data=f"forceclose {videoid}|{user_id}",
             ),
         ],
@@ -58,11 +58,11 @@ def stream_quality_markup(videoid, duration, user_id):
         ],
         [
             InlineKeyboardButton(
-                text="❮ 𝙶𝙾 𝙱𝙰𝙲𝙺​",
+                text="< ʙᴀᴄᴋ​",
                 callback_data=f"gback_list_chose_stream {videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="⍟ 𝙲𝙻𝙾𝚂𝙴 ⍟",
+                text="ᴄʟᴏꜱᴇ",
                 callback_data=f"forceclose {videoid}|{user_id}",
             ),
         ],
